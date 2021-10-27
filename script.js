f44c2e105028de5c441eeb9363169b12fd4cb68b
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-27 17:57:20 +0200

    JS Completed

--- script.js
+++ script.js
@@ -7,20 +7,50 @@ let arrayNumeri = [];
 
 while (arrayNumeri.length < 5) {
 
-  let numeroRandom = generateRandom(1, 100);
+  const numeroRandom = generateRandom(1, 100);
 
-  let numeroVerificato = arrayNumeri.includes(numeroRandom)
+  const numeroVerificato = arrayNumeri.includes(numeroRandom);
 
   if (!numeroVerificato) {
     arrayNumeri.push(numeroRandom);
   }
-}
-console.log("Array numeri Random", arrayNumeri.sort(function(a,b){return a-b}));
+};
+console.log("Array numeri Random", arrayNumeri.sort(function (a, b) { return a - b }));
 
 
 
 
-alert("Memorizza questi numeri: \n" + arrayNumeri);
+
+alert("Memorizza questi numeri: \n" + arrayNumeri.join(", "));
+
+setTimeout(richiestaNumeri, 3000);
+console.log("Timer di 3 Secondi come prova");
 
 
 
+
+let arrayPrompt = ["primo", "secondo", "terzo", "quarto", "quinto"];
+let numeriUtente = [];
+
+
+function richiestaNumeri() {
+  for (let i = 0; i < 5; i++) {
+
+    const richiestaNumero = parseInt(prompt('Inserisci il ' + arrayPrompt[i] + ' numero.'));
+
+    const richiestaNumeroVerificato = numeriUtente.includes(richiestaNumero);
+
+    const arrayNumeriVerificato = arrayNumeri.includes(richiestaNumero);
+
+    if (!richiestaNumeroVerificato && arrayNumeriVerificato) {
+      numeriUtente.push(richiestaNumero);
+    }
+  }
+  console.log("Array numeri indovinati", numeriUtente.sort(function (a, b) { return a - b }));
+
+  if (numeriUtente.length === 0) {
+    console.log(`Hai memorizzato ` + numeriUtente.length + ` numeri.`);
+  } else {
+    console.log(`Hai memorizzato ` + numeriUtente.length + ` numeri e sono: ` + numeriUtente.join(", "))
+  }
+};
